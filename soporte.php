3d99e4a2ce0b031bfd91f319971d04d00724f5ea
arturo Ledezma Mata <arturo.ledezmam@uanl.edu.mx>
2025-05-07 00:59:26 -0600

    Creación de la interfaz de soporte que contiene información sobre nosotros y nuestro contacto

--- soporte.php
+++ soporte.php
@@ -1,12 +1,20 @@
 <!--La linea 2 representa el inicio de la estructura html5 y el navbar. includes/header.php -->
 <?php include("includes/header.php"); ?>
 <!--Inicio de Body -->
+<div style="display: flex; justify-content: center; gap: 30px; margin-top: 40px;">
+<div style ="background-color: white; color: black; padding: 20px; border-radius: 15px; width: 450px; margin: 80px auto;">
+    <h2>¿Quiénes somos?</h2>
+    <p>Temach Gaming es una comunidad creada por y para gamers. Nos apasionan los videojuegos, el trabajo en equipo y brindar un espacio donde puedas informarte, conectarte y crecer dentro del mundo gaming. ¡Tu aventura comienza aquí!     
+    </p>
+</div>  
 
-
-
-
-
-
+<div style ="background-color: white; color: black; padding: 20px; border-radius: 15px; width: 450px; margin: 80px auto;">
+    <h2>Contáctanos</h2>
+    <p>+52 844 503 9958</p>
+    <p> tematchgaming14@gmail.com.mx</p>
+    </div>    
+</div>
+<div style="height: 200px;"></div>
 
 <!-- Fin de Body -->
 <?php include("includes/footer.php"); ?>
