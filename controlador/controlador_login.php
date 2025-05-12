69e9283e15131d907b3cc8a9233765d3e12af0a7
Juan Pablo García Monreal <pablo.garciam@uanl.edu.mx>
2025-05-11 23:38:49 -0600

    Creacion de la interfaz de login con conexion a la base de datos

---
+++ controlador/controlador_login.php
@@ -0,0 +1,26 @@
+<?php
+if (!empty($_POST["iniciar"])) {
+    if (empty($_POST["correo"]) and empty($_POST["contrasena"])) {
+        echo '<div class="alert alert-danger">Debe llenar todos los campos</div>';
+    } else {
+        $correo = $_POST["correo"];
+        $contraseña = $_POST["contrasena"];
+
+        $sql = $conexion->query("SELECT * FROM usuarios WHERE correo = '$correo' AND contrasena = '$contraseña'");
+
+        if ($datos = $sql->fetch_object()) {
+            $_SESSION["id"] = $datos->id;
+            $_SESSION["rol"] = $datos->id_rol;
+
+            if ($datos->id_rol == 1) {
+                header("Location: admin/vista_admin.php");
+            } else {
+                header("Location: index.php");
+            }
+            exit(); 
+        } else {
+            echo '<div class="alert alert-danger">Acceso denegado</div>';
+        }
+    }
+}
+?>
